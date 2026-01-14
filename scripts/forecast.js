1c9d45f7a4497956512b75674e1d3b7dd8b0df3b
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-14 18:07:49 +0100

    Add API key and implement weather data fetching in forecast.js; update image source in index.html

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,12 +1,29 @@
-const key = "";
+const key = "zpka_fe5943feb5ef488cbc09bee5273f40a2_d08785";
 
+//get weather information
+const getWeather = async (id) => {
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//get city information
 const getCity = async (city) => {
   const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
-  console.log(data[0]);
+  return data[0];
 };
 
-getCity("manchester");
+getCity("manchester")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
