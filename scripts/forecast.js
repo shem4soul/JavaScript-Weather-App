79028702c5a9cdaf0be8873c34d0cd3e2be165e6
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-15 17:56:28 +0100

    Remove example city fetching and weather logging from forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -18,12 +18,3 @@ const getCity = async (city) => {
   const data = await response.json();
   return data[0];
 };
-
-getCity("manchester")
-  .then((data) => {
-    return getWeather(data.Key);
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => console.log(err));
