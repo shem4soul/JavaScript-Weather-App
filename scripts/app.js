8e6f5eb48a87c29851ad58f7b3a41846a8ad0158
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-15 17:44:11 +0100

    Refactor city form submission handling in app.js; enhance updateCity function

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,17 @@
 const cityForm = document.querySelector("form");
 
+const updateCity = async (city) => {
+  console.log(city);
+};
+
 cityForm.addEventListener("submit", (e) => {
   //prevent default action
   e.preventDefault();
+
+  //get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //update the UI with new city
+  updateCity(city);
 });
