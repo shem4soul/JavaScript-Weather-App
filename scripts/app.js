af11da9f79d537ca655bb51bf726bbcf531f55a3
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-22 18:16:40 +0100

    added image files

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,30 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+  // console.log(data);
+
+  // const cityDetails = data.cityDetails;
+  // const weather = data.weather;
+
+  const { cityDetails, weather } = data;
+
+  //update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3 temp">${weather.WeatherText}</div>
+     <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  </div>
+`;
+
+  //remove the d-none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
@@ -17,6 +43,6 @@ cityForm.addEventListener("submit", (e) => {
 
   //update the UI with new city
   updateCity(city)
-    .then((data) => console.log(data))
+    .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 });
