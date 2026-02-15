4a8523d1b07ef3a97eaf1b852399d3939b6f323e
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-15 18:56:28 +0100

    Initial clean commit

---
+++ config.js
@@ -0,0 +1,1 @@
+const API_KEY = "zpka_fe5943feb5ef488cbc09bee5273f40a2_d08785be";
---
+++ scripts/forecast.js
@@ -0,0 +1,48 @@
+class Forecast {
+  constructor() {
+    this.key = "";
+    this.weatherURI =
+      "https://dataservice.accuweather.com/currentconditions/v1/";
+    this.cityURI =
+      "https://dataservice.accuweather.com/locations/v1/cities/search";
+  }
+
+  async updateCity(city) {
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    return { cityDets, weather };
+  }
+  async getCity(city) {
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getWeather(id) {
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+}
+
+// const key = "";
+
+// //get weather information .
+// const getWeather = async (id) => {
+//   const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+//   const query = `${id}?apikey=${key}`;
+//   const response = await fetch(base + query);
+//   const data = await response.json();
+//   return data[0];
+// };
+
+// //get city information
+// const getCity = async (city) => {
+//   const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
+//   const query = `?apikey=${key}&q=${city}`;
+
+//   const response = await fetch(base + query);
+//   const data = await response.json();
+//   return data[0];
+// };
